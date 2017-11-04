84fb643bbd5dcca1c0309df20fe8ce80cd4ab94c
Aahan Singh <aahansingh95@gmail.com>
2017-11-04 17:24:02 +0800

    Init h,w for mean values

--- a2/assignment2.py
+++ a2/assignment2.py
@@ -20,7 +20,9 @@ def main():
     fileName = argv[2]
     inputData = np.rollaxis(imread(fileName), 2, 0)
     channels, height, width = inputData.shape
-
+    h = np.random.randint(low=0,high=height)
+    w = np.random.randint(low=0, high=height)
+    pix = inputData[h,w,:]
 
 if __name__ == "__main__":
     main()
