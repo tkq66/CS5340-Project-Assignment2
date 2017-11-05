004270cd9a54f2823464952f8193888c053930e0
Aahan Singh <aahansingh95@gmail.com>
2017-11-05 18:28:40 +0800

    Fixed Expectation function. Added Maximization Function

--- a2/assignment2.py
+++ a2/assignment2.py
@@ -38,15 +38,54 @@ def expectation(x, mean, cov, mixCoeff):
     N = np.array(N)
     N = N.T
     numerator = mixCoeff * N
-    denominator = numerator.sum(axis=0)
-    responsibility = numerator / denominator
-
+    denominator = numerator.sum(axis=1) # for each N add values of K
+    denominator = denominator.reshape(1,-1) # convert to row vector
+    responsibility = numerator / denominator.T # denominator.T is a column vector thus can numerator divide by column
+    print(denominator.shape)
     return responsibility
 
 
-def maximization():
-    maximization = None
-    return maximization
+def maximization(responsibility, x, means, ):
+    new_cov = None
+    new_mixcoeff = None
+    N_k = responsibility.sum(axis=0) # for each K add values of N
+    x = x.reshape(responsibility.shape[0],-1)
+    print(x.shape)
+    # CALCULATING NEW MEANS
+    new_means = []
+
+    for i in range(responsibility.shape[1]): # for each k
+        numerator = 0
+        for index,pix in enumerate(x):
+            numerator+= responsibility[index,i]*pix
+        denominator = N_k[i]
+        value = numerator/denominator
+        new_means.append(value)
+
+    new_means = np.array(new_means)
+
+    # CALCULATING NEW COVARIANCES
+    new_cov = []
+    for i in range(responsibility.shape[1]): # for each k
+        numerator = 0
+        for index,pix in enumerate(x):
+            temp = np.array(pix-new_means[i])
+            temp = temp.reshape(1,-1)
+            temp = responsibility[index,i] * temp * temp.T
+            numerator+= temp
+        denominator = N_k[i]
+        value = numerator/denominator
+        new_cov.append(value)
+    new_cov = np.array(new_cov)
+
+    # CALCULATING NEW MIX COEF
+    new_mixcoeff = []
+    N = responsibility.shape[0]
+    for i in N_k:
+        new_mixcoeff.append(i/N)
+    new_mixcoeff = np.array(new_mixcoeff)
+
+    return new_means, new_cov, new_mixcoeff
 
 
 def evaluateLogLikelihood(inputData, mean, cov, mixCoeff, k):
@@ -67,8 +106,17 @@ def main():
 
     inputData = imread(fileName)
     initMean, initCov, initMixCoeff, initLogLikelihood = initialize(k, inputData)
+    print('init means=', initMean.shape)
+    print('init cov=', initCov.shape)
+    print('init mixcoef=', initMixCoeff.shape)
     responsibility = expectation(inputData, initMean, initCov, initMixCoeff)
+    new_means, new_cov, new_mix = maximization(responsibility,inputData,initMean)
+    print('new means=',new_means.shape)
+    print('new cov=', new_cov.shape)
+    print('new mixcoef=', new_mix.shape)
 
+    print('old=\n',initMean)
+    print('\nnew=\n',new_means)
 
 if __name__ == "__main__":
     main()
