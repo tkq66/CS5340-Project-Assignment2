bbf21c4369808dcaf7a5d4ca0e85202a0ff50a14
TK <teekayu8@hotmail.com>
2017-11-06 04:13:23 +0800

    Add training loop, handle case when covariance became singular matrix, break off initialize function to become more modular

--- a2/assignment2.py
+++ a2/assignment2.py
@@ -4,27 +4,33 @@ from scipy.stats import multivariate_normal
 from sys import argv
 
 
-def initialize(k, inputData):
-    height, width, channels = inputData.shape
-
+def initializeMean(k, height, width, inputData):
     # Initialize the mean
     h = np.random.choice(np.arange(height), k, replace=False)
     w = np.random.choice(np.arange(width), k, replace=False)
-    initMean = inputData[h, w, :]
+    return inputData[h, w, :]
 
+
+def initializeCov(k, channels, spread=50):
     # Initialzie covariance
-    initCov = np.random.uniform(0, 50, size=(k, channels, channels))
+    initCov = np.random.uniform(0, spread, size=(k, channels, channels))
     for i, x in enumerate(initCov):
         x = x.T.dot(x)
         initCov[i] = x
+    return initCov
 
+
+def initializeMix(k):
     # Initialize mixing coefficient
-    initMixCoeff = np.repeat((1 / k), k)
+    return np.repeat((1 / k), k)
 
-    # Calculate log likelihood for the init values
-    initLogLikelihood = evaluateLogLikelihood(inputData, initMean, initCov, initMixCoeff, k)
 
-    return initMean, initCov, initMixCoeff, initLogLikelihood
+def initialize(k, inputData):
+    height, width, channels = inputData.shape
+    initMean = initializeMean(k, height, width, inputData)
+    initCov = initializeCov(k, channels)
+    initMixCoeff = initializeMix(k)
+    return initMean, initCov, initMixCoeff
 
 
 def expectation(k, x, mean, cov, mixCoeff):
@@ -35,13 +41,13 @@ def expectation(k, x, mean, cov, mixCoeff):
 
     numerator = np.empty((k, totalPixels))
     for i in range(k):
-        numerator[i] = mixCoeff[i] * multivariate_normal.pdf(data, mean=mean[i, :], cov=cov[i, :])
+        numerator[i] = mixCoeff[i] * multivariate_normal.pdf(data, mean=mean[i], cov=cov[i])
     denominator = numerator.sum(axis=0)  # for each N add values of K
     responsibility = numerator / denominator  # denominator.T is a column vector thus can numerator divide by column
     return responsibility
 
 
-def maximization(k, responsibility, x, means, ):
+def maximization(k, responsibility, x, means):
     h, w, c = x.shape
     totalPixels = h * w
     data = x.reshape(totalPixels, c)
@@ -67,7 +73,7 @@ def maximization(k, responsibility, x, means, ):
     return new_means, new_cov, new_mixCoeff
 
 
-def evaluateLogLikelihood(inputData, mean, cov, mixCoeff, k):
+def evaluateLogLikelihood(k, inputData, mean, cov, mixCoeff):
     height, width, channels = inputData.shape
     formattedInput = inputData.reshape(height * width, channels).astype(np.float64)
 
@@ -83,10 +89,26 @@ def main():
     k = int(argv[1])
     fileName = argv[2]
 
+    # Train data, assuming convergence criteria is logLikelihood = 0
+    logLikelihood = 1000000
     inputData = imread(fileName)
-    initMean, initCov, initMixCoeff, initLogLikelihood = initialize(k, inputData)
-    responsibility = expectation(k, inputData, initMean, initCov, initMixCoeff)
-    new_means, new_cov, new_mix = maximization(k, responsibility, inputData, initMean)
+    height, width, channels = inputData.shape
+    old_mean, old_cov, old_mix = initialize(k, inputData)
+    i = 0
+    while logLikelihood != 0:
+        try:
+            logLikelihood = evaluateLogLikelihood(k, inputData, old_mean, old_cov, old_mix)
+        except np.linalg.LinAlgError:
+            print("New covariance matrix is singular. Resetting...")
+            old_cov = initializeCov(k, channels)
+            continue
+        print(i, logLikelihood)
+        responsibility = expectation(k, inputData, old_mean, old_cov, old_mix)
+        mean, cov, mix = maximization(k, responsibility, inputData, old_mean)
+        old_mean = mean
+        old_cov = cov
+        old_mix = mix
+        i += 1
 
 
 if __name__ == "__main__":
